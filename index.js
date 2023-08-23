6e85391bf794f71aee3bfb8aa703436b0fff5160
william lee <will.lee.npc@gmail.com>
2023-08-23 05:10:48 -0400

    initial

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+/*
+const newHeader = document.createElement("h1");
+const nodeName = "victory"
+newHeader.nodeName
+document.body.append(newHeader);
+//newHeader.append(h1);
+newHeader.id="victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+*/
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
